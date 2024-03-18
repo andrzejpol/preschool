7aef8d71136c4104d692ea705efa1d98c8eab014
Andrzej Polak <andrzejp2805@gmail.com>
2024-03-18 21:28:21 +0100

    Setup environment

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { StyledEngineProvider } from "@mui/material";
+
+ReactDOM.createRoot(document.querySelector("#root")!).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
