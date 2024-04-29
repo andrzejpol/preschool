20de07334ca329d17b2c887c4d6d37f7eae956d4
Andrzej Polak <andrzejp2805@gmail.com>
2024-04-29 21:11:57 +0200

    Create Routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Root from "./components/Root";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        path:"contacts/:contactId",
+        element:<p>Hello</p>
+      }
+    ]
+  },
+  {
+    path:"/login",
+    element: <p>Login page</p>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path:"/register",
+    element: <p>Register page</p>
+  },
+  {
+    path:"/logout",
+    element: <p>Logout page</p>
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
